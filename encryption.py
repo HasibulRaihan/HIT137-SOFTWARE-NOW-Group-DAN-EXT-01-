78e9d4cfda591daaf052b8a1578362b6a44a41d5
Jeshika-code <jeshikasapkota5@gmail.com>
2025-12-17 12:59:31 +0930

    Initial

---
+++ encryption.py
@@ -0,0 +1,17 @@
+# Group Name: DAN/EXT 01
+# Group Members: 4
+# Member 1: Md Hasibul Raihan - S397592
+# Member 2: Tanisa Sanam Vabna - S397593
+# Member 3: JESHIKA SAPKOTA - S399269
+# Member 4: LADDA DAWSON - S382273
+
+#Create a program that reads the text file "raw_text.txt", encrypts its contents using a simple encryption method, and writes the encrypted text to a new file "encrypted_text.txt". Then create a function to decrypt the content and a function to verify the decryption was successful.
+
+# ----------------------- MAIN PROGRAM START -----------------------
+
+def encrypt_letter(ch, shift1, shift2):
+    #lowercase letters
+   if 'a'<=ch<='z':
+
+
+        
